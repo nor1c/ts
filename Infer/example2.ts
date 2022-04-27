2d98b23bc4aa101157361abd792c2fc0ada52590
Ahmad Fauzi <ahmad.uji1902@gmail.com>
2022-04-27 09:49:31 +0700

    update

--- Infer/example2.ts
+++ Infer/example2.ts
@@ -1,7 +1,6 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
-// Example: Get a type inside a wrapped type.
-
+// example 1: Get a type inside Promise wrapper
 type MyAwaited<T> = T extends Promise<infer R> ? R : T
 
 type X = Promise<string>
@@ -14,11 +13,19 @@ type cases = [
   Expect<Equal<MyAwaited<Z>, string | number>>
 ] // Passed!
 
-// Nested Promise
+// example 2: Nested Promise
 type MyAwaitedNested<T> = T extends Promise<infer R> ? MyAwaitedNested<R> : T
 
 type ZZ = Promise<Promise<string>>
 
 type nestedCase = [
   Expect<Equal<MyAwaitedNested<ZZ>, string>>
+] // Passed!
+
+// example 3: Get a base type of array type
+type NS = number[]
+type TNumber<T> = T extends (infer R)[] ? R : T
+
+type arrayCase = [
+  Expect<Equal<TNumber<NS>, number>>
 ] // Passed!
---
+++ README.md
@@ -0,0 +1,6 @@
+#### Learning Resources
+
+- TypeScript Official Documentation: https://www.typescriptlang.org/docs/
+- TypeScript Clean Code Principles: https://github.com/labs42io/clean-code-typescript
+- TypeScript Deep Dive by Basarat: https://basarat.gitbook.io/typescript/
+- TypeScript Challenges: https://tsch.js.org/
